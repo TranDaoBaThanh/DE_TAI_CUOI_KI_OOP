8ea9925135135e7f261a245e89941545b5a2b60c
Trần Đào Bá Thành <thanhtrandaoba@gmail.com>
2024-04-22 21:18:35 +0700

    them class

--- TicTacToe/TicTacToe/main.cpp
+++ TicTacToe/TicTacToe/main.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Board {
+private:
+
+public:
+};
+
+class Player {
+private:
+
+public:
+};
+
+class HumanPlayer : public Player {
+private:
+
+public:
+};
+
+class ComputerPlayer : public Player {
+private:
+
+public:
+};
+
+class Game {
+private:
+
+public:
+};
+
+
 int main() {
-	cout << "Hello World\n";
 	return 0;
 }
